64b629166441dfc3b15f3ff00e55e8b6a46c1fd5
Ann Sellén <brevtillann@hotmail.com>
2019-12-19 11:29:26 +0100

    changes in basic

--- basic.js
+++ basic.js
@@ -54,6 +54,39 @@ function galleriesPage() { // opens a new "galleries" content page/screen
   document.getElementById("demo").innerHTML = "Galleries";
 }
 
+//Create the form
+var form = document.createElement('form');
+form.id="myForm";
+form.action="results.html";
+form.method="GET";
+document.body.appendChild(form);
+
+//Create the div 
+var div1 = document.createElement("div");
+div1.id="aDiv"; 
+document.body.appendChild(div1);
+
+//Place the input field 
+var input = document.createElement("input");
+input.type = "url";
+input.id = "urlinput";
+input.placeholder = "Enter URL Here";
+input.value = "";
+document.getElementById("myForm").appendChild(input);
+var url = document.getElementById("urlinput").value;
+
+//Add submit button 
+var inputbutton = document.createElement("input");
+inputbutton.id = "submitbutton";
+inputbutton.type = "submit";
+inputbutton.value = "submit";
+document.getElementById("myForm").appendChild(inputbutton);  //This line doesnt work
+inputbutton.addEventListener("click", clearForm);
+
+//Clears a form, clear image form
+function clearForm() {
+    form.reset();
+}
 
 //--------------------------------
 // changes h3 heading with button click,, Temporary!!!!
