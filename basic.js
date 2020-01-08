967312a8ab46960e166239418358851b3d2282ad
”Maria <thea_79@hotmail.com>
2020-01-08 10:42:25 +0100

    added form validation

--- basic.js
+++ basic.js
@@ -119,35 +119,95 @@ inputbutton.addEventListener("click", function() {});
 }
 
 
+//--------------------------------------------------------------
+/**
+ * @description with a click the login form is displayed.
+ */
 
-// login button
 function loginBtn(){
-  // create the login button
   var logInButton = document.createElement('button');
   logInButton.textContent="Login";
-  logInButton.id="loginbutton"
+  logInButton.id="loginbtn"
   document.body.appendChild(logInButton);
   logInButton.addEventListener('click', login, false)
   
 
   /**
-   * @description shows the login form
+   * @description contains the entire login form
    *  
    */
-  function login(e){
+  function login(){
     
     var loginDiv = document.createElement('div');
     
     var logInForm = document.createElement('form');
-    logInForm.id="loginform";
-    logInForm.method='Get';
+     logInForm.id="loginform";
+     logInForm.method ='Get';
+     logInForm.action='#'    
+     logInForm.addEventListener('submit',function(event){
+  
+      
+       event.preventDefault(); //prevents page reloading
+       var feedback ="";
+
+       var email = document.getElementById('email').value; 
+      
+      // indexof is a method. here i want to find out the position of @
+        var atpos = email.indexOf('@'); 
+      
+      // lastindexof means the last dot if there are more
+        var atdot = email.lastIndexOf('.'); 
+      
+        var password = document.getElementById('password').value;
+        var plength = password.length;
+
+        //this is a password for a pretend user to see if it works
+        var user1Password="aaaaaaa"; 
+      
+        
+        if(atpos < 1 || atdot < 1){
+          feedback +=" <strong style='color:#C00'>The e-mail address is not correct it is missing an @ or a dot. <br></strong>" ;
+          document.getElementById('email').style.borderColor='red';
+          
+        }
+        else if (email != atpos < 1 || atdot < 1) {
+          document.getElementById('email').style.borderColor='lightskyblue';
+        }
+
+        if(plength < 7 ){
+          feedback +=" <strong style='color:#C00'>Your password should be longer than 7 characters.</strong>" ;
+          document.getElementById('password').style.borderColor='red';
+        }
+        // om min atpos (@ position) är störren än atdock (. position))
+        // alltså om @ ligger efter . då har vi ett problem
+        else if (atpos > atdot){
+          feedback +=" <strong style='color:#C00'>Email address is not correct @ must be before last dot</strong>" ;
+        }
+        else if(password != user1Password){
+          feedback +=" <strong style='color:#C00'>The user does not exist.</strong>" ;
+          console.log("hej");
+        }
+       
+        else{
+          document.getElementById('email').style.borderColor='lightskyblue';
+          document.getElementById('password').style.borderColor='lightskyblue';
+        
+
+        }
+      
+        document.getElementById('feedback').innerHTML = feedback;
+         return false;
+     
+       
+     })
     
     // input field email
     var emailInput = document.createElement('input');
     emailInput.id='email';
-    emailInput.setAttribute("type", "email");
+    emailInput.setAttribute("type", "text"); 
     emailInput.placeholder='E-mail';
-    emailInput.maxLength='20';
+    
+    
     
     var lineBreak = document.createElement('br');
     
@@ -156,8 +216,7 @@ function loginBtn(){
     passwordInput.id='password';
     passwordInput.placeholder='password'
     passwordInput.setAttribute("type", "password");
-    passwordInput.maxLength='20';
-
+ 
     
     // creates the submit button inside the form
     var loginSubmitButton = document.createElement('button');
@@ -165,30 +224,40 @@ function loginBtn(){
     loginSubmitButton.type='submit';
     loginSubmitButton.value='submit';
     loginSubmitButton.textContent='Submit';
-    loginSubmitButton.addEventListener('click',submit, false)
 
-    function submit (){
-      // here the code should be for where the form should be sent.
-    }
-
+    var createAccount = document.createElement('p')
+    createAccount.id='account'
+    createAccount.textContent="Create Account"
+    
+    let feedBack = document.createElement('p');
+    feedBack.id='feedback';
+    
+    document.body.appendChild(feedBack);
+    
+      
     document.body.appendChild(loginDiv); // Div 
     loginDiv.appendChild(logInForm);    // form
     logInForm.appendChild(emailInput); // email input
     logInForm.appendChild(lineBreak);   // line break
     logInForm.appendChild(passwordInput); // password input
     logInForm.appendChild(loginSubmitButton); // submit button
-
+    logInForm.appendChild(createAccount); // text create account
+    
     
 
-    // ta bort eventlyssnaren så det bara blir ett formulär även om man trycker på knappen mer än en gång
-    this.removeEventListener('click',login,false)
+    // makes it possible to press the button several times but it only shows one form
+    this.removeEventListener('click',login ,false)
     
     }
     
   }
   
+  
    loginBtn();
   
+ 
+
+
 
 
 
